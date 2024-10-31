8998d621d82f47fd95b32477fef8d77929295bbe
Hennry117 <zhahy141@mymail.unisa.edu.au>
2024-11-01 04:00:30 +1030

    updating passenger code

--- Cloud code/src/nuber/students/Passenger.java
+++ Cloud code/src/nuber/students/Passenger.java
@@ -1,15 +1,26 @@
-package nuber.students;
+package nuber.students; // Define the package of this class
 
-public class Passenger extends Person
-{
-	
+/**
+ * The Passenger class represents a passenger, extending the Person class.
+ */
+public class Passenger extends Person {
+
+	/**
+	 * Constructor to create a new Passenger instance.
+	 *
+	 * @param name     The name of the passenger.
+	 * @param maxSleep The maximum sleep time (travel time) for the passenger.
+	 */
 	public Passenger(String name, int maxSleep) {
-		super(name, maxSleep);
+		super(name, maxSleep); // Call the constructor of the parent class, Person
 	}
 
-	public int getTravelTime()
-	{
-		return (int)(Math.random() * maxSleep);
+	/**
+	 * Get the travel time for the passenger.
+	 *
+	 * @return The travel time of the passenger, a random integer between 0 and maxSleep.
+	 */
+	public int getTravelTime() {
+		return (int) (Math.random() * maxSleep); // Generate and return a random travel time
 	}
-
 }

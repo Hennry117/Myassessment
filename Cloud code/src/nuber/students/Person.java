875d1613331fafc4920c28924fd8ccb6d4d84d8e
Hennry117 <zhahy141@mymail.unisa.edu.au>
2024-11-01 13:56:55 +1030

    Updating code

--- Cloud code/src/nuber/students/Person.java
+++ Cloud code/src/nuber/students/Person.java
@@ -1,24 +1,93 @@
-package nuber.students;
+package nuber.students; // Define the package this class belongs to
 
-public abstract class Person
-{
-	
-	public final static String[] SAMPLE_NAMES = {"Bryan","Olivia","Vincent","Kenneth","Debra","Jack","Harold","Isabella","Jerry","Stephen","Larry","Ruth","Diane","Gerald","Brandon","Virginia","Helen","Gary","Noah","Michell","Alexis","Zachary","Gregory","Arthur","Dennis","Terry","Rose","Jeffrey","Jean","Jane","Brenda","Louis","Mary","Julia","Sandra","Catherine","Adam","Samantha","Amber","Ralp","Jacob","Raymond","Rachel","Kelly","Danielle","John","Melissa","Albert","Brian","Eugne","Jeremy","Nathan","Beverly","Margaret","Natalie","Charlotte","Ann","Betty","Randy","Tyler","Emma","Willie","Charles","Lisa","Anthony","Sara","Sean","James","Johnny","Jud","Evelyn","Theresa","Gloria","Emily","Denise","Frank","Steven","Jacqueline","Diana","Ronald","Kayla","Joe","Nicole","Scott","Henry","Lawrence","Ethan","Stephanie","Kevin","Kathleen","Angela","Joyce","Sarah","Benjamin","Carl","Cynthia","Nicholas","Andrea","Robert","Martha","Susan","Ryan","Alexander","Donna","Thomas","Brittany","Timothy","Hannah","Heather","Linda","Joan","Pamela","Maria","Kyle","Logan","Paul","Andrew","Dylan","Christina","Kimberly","Patricia","Victoria","Philip","Shirley","Billy","Jonathan","Roy","Christopher","Roger","Anna","Richard","Doris","Bruce","Peter","Dorothy","Amanda","Marilyn","Christine","Marie","Karen","Jordan","Wayne","Edward","Justin","Walter","Rebecca","Sharon","Jesse","Joshua","Sophia","Grace","Deborah","Ashley","Joseph","Matthew","Alan","Julie","Abigail","Mark","Megan","Juan","Michael","Frances","George","Eric","William","Cheryl","Daniel","Katherine","Amy","Laura","Donald","Jennifer","Judith","Carolyn","Christian","Janice","Barbara","Elijah","Nancy","Aaron","Teresa","Bobby","Douglas","Russell","Jose","Keith","Kathryn","Samuel","Austin","Jason","Jessica","David","Lauren","Patrick","Gabriel","Alice","Elizabeth","Madison","Carol"};
+/**
+ * The Person class represents a person with basic attributes and methods.
+ * This class is abstract and meant to be inherited by other classes, such as Passenger.
+ */
+public abstract class Person {
+
+	// A sample array of names to be used for random name generation
+	public final static String[] SAMPLE_NAMES = {
+			"Bryan", "Olivia", "Vincent", "Kenneth", "Debra", "Jack",
+			"Harold", "Isabella", "Jerry", "Stephen", "Larry", "Ruth",
+			"Diane", "Gerald", "Brandon", "Virginia", "Helen", "Gary",
+			"Noah", "Michell", "Alexis", "Zachary", "Gregory", "Arthur",
+			"Dennis", "Terry", "Rose", "Jeffrey", "Jean", "Jane", "Brenda",
+			"Louis", "Mary", "Julia", "Sandra", "Catherine", "Adam",
+			"Samantha", "Amber", "Ralp", "Jacob", "Raymond", "Rachel",
+			"Kelly", "Danielle", "John", "Melissa", "Albert", "Brian",
+			"Eugne", "Jeremy", "Nathan", "Beverly", "Margaret", "Natalie",
+			"Charlotte", "Ann", "Betty", "Randy", "Tyler", "Emma", "Willie",
+			"Charles", "Lisa", "Anthony", "Sara", "Sean", "James", "Johnny",
+			"Jud", "Evelyn", "Theresa", "Gloria", "Emily", "Denise", "Frank",
+			"Steven", "Jacqueline", "Diana", "Ronald", "Kayla", "Joe",
+			"Nicole", "Scott", "Henry", "Lawrence", "Ethan", "Stephanie",
+			"Kevin", "Kathleen", "Angela", "Joyce", "Sarah", "Benjamin",
+			"Carl", "Cynthia", "Nicholas", "Andrea", "Robert", "Martha",
+			"Susan", "Ryan", "Alexander", "Donna", "Thomas", "Brittany",
+			"Timothy", "Hannah", "Heather", "Linda", "Joan", "Pamela",
+			"Maria", "Kyle", "Logan", "Paul", "Andrew", "Dylan", "Christina",
+			"Kimberly", "Patricia", "Victoria", "Philip", "Shirley",
+			"Billy", "Jonathan", "Roy", "Christopher", "Roger", "Anna",
+			"Richard", "Doris", "Bruce", "Peter", "Dorothy", "Amanda",
+			"Marilyn", "Christine", "Marie", "Karen", "Jordan", "Wayne",
+			"Edward", "Justin", "Walter", "Rebecca", "Sharon", "Jesse",
+			"Joshua", "Sophia", "Grace", "Deborah", "Ashley", "Joseph",
+			"Matthew", "Alan", "Julie", "Abigail", "Mark", "Megan",
+			"Juan", "Michael", "Frances", "George", "Eric", "William",
+			"Cheryl", "Daniel", "Katherine", "Amy", "Laura", "Donald",
+			"Jennifer", "Judith", "Carolyn", "Christian", "Janice",
+			"Barbara", "Elijah", "Nancy", "Aaron", "Teresa", "Bobby",
+			"Douglas", "Russell", "Jose", "Keith", "Kathryn", "Samuel",
+			"Austin", "Jason", "Jessica", "David", "Lauren", "Patrick",
+			"Gabriel", "Alice", "Elizabeth", "Madison", "Carol"
+	};
+
+	// Index for the next random name
 	private static int nextNameIndex = 0;
-	
+
+	// The person's name
 	public String name;
+
+	// Maximum sleep time for the passenger
 	protected int maxSleep;
-	
-	public Person(String name,int maxSleep) {
-		this.name = name;
-		this.maxSleep = maxSleep;
-	}
-	
-	public static String getRandomName()
-	{
-		nextNameIndex = ++nextNameIndex % SAMPLE_NAMES.length;
-		
-		return SAMPLE_NAMES[nextNameIndex];
+
+	/**
+	 * Constructor to create a new Person instance.
+	 *
+	 * @param name     The name of the person
+	 * @param maxSleep The maximum sleep time
+	 */
+	public Person(String name, int maxSleep) {
+		this.name = name; // Initialize the name
+		this.maxSleep = maxSleep; // Initialize the maximum sleep time
 	}
 
+	/**
+	 * Get the name of the person.
+	 *
+	 * @return The name of the person
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * Get the maximum sleep time for the passenger.
+	 *
+	 * @return The maximum sleep time
+	 */
+	public int getMaxSleep() {
+		return maxSleep;
+	}
+
+	/**
+	 * Get a random name, cycling through the SAMPLE_NAMES array.
+	 *
+	 * @return A random name
+	 */
+	public static String getRandomName() {
+		nextNameIndex = ++nextNameIndex % SAMPLE_NAMES.length; // Update the index and ensure it's within array bounds
+		return SAMPLE_NAMES[nextNameIndex]; // Return the next random name
+	}
 }
--- Cloud code/src/nuber/students/Simulation.java
+++ Cloud code/src/nuber/students/Simulation.java
@@ -6,95 +6,102 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Random;
-import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.Future;
 
 public class Simulation {
 
-	/**
-	 * 
-	 * @param regions The region names and maximum simultaneous active bookings allowed in that region
-	 * @param maxDrivers The number of drivers to create
-	 * @param maxPassengers The number of passengers to create
-	 * @param maxSleep The maximum amount a thread will sleep (in millseconds)) to simulate driving to, or dropping off a passenger
-	 * @param logEvents Whether to log booking events to the console
-	 * @throws Exception
-	 */
-	public Simulation(HashMap<String, Integer> regions, int maxDrivers, int maxPassengers, int maxSleep, boolean logEvents) throws Exception {
-		
-		//store the current time
-		long start = new Date().getTime();
-		
-		//print some space in the console
-		System.out.println("\n\n\n");
+    /**
+     * Constructor for the Simulation class.
+     *
+     * @param regions       The region names and maximum simultaneous active bookings allowed in that region.
+     * @param maxDrivers    The number of drivers to create.
+     * @param maxPassengers The number of passengers to create.
+     * @param maxSleep      The maximum amount a thread will sleep (in milliseconds) to simulate driving to or dropping off a passenger.
+     * @param logEvents     Whether to log booking events to the console.
+     * @throws Exception    Initializes the simulation with the given parameters, setting up bookings and dispatching drivers.
+     */
+    public Simulation(HashMap<String, Integer> regions, int maxDrivers, int maxPassengers, int maxSleep, boolean logEvents) throws Exception {
 
-		//store a queue of all current bookings as Future's that will eventually give us back a BookingResult object
-		Queue<Future<BookingResult>> bookings = new LinkedList<Future<BookingResult>>();
+        // Store the current time
+        long start = new Date().getTime();
 
-		//convert the region names from the regions map into an array
-		String[] regionNames = regions.keySet().toArray(new String[0]);
+        // Print some space in the console
+        System.out.println("\n\n\n");
 
-		//create a new dispatch object
-		NuberDispatch dispatch = new NuberDispatch(regions, logEvents);
+        // Queue to store all current bookings as Future objects that will eventually give back a BookingResult object
+        Queue<Future<BookingResult>> bookings = new LinkedList<>();
 
-		// create drivers that are available for jobs
-		for (int i = 0; i < maxDrivers; i++) {
-			Driver d = new Driver("D-" + Person.getRandomName(), maxSleep);
-			dispatch.addDriver(d);
-		}
+        // Convert the region names from the regions map into an array
+        String[] regionNames = regions.keySet().toArray(new String[0]);
 
-		// create passengers
-		for (int i = 0; i < maxPassengers; i++) {
-			
-			Passenger p = new Passenger("P-" + Person.getRandomName(), maxSleep);
-			
-			//choose a random region to assign this person
-			String randomRegion = regionNames[new Random().nextInt(regionNames.length)];
-			
-			//add each passenger to dispatch to book their travel for a random region
-			Future<BookingResult> f = dispatch.bookPassenger(p, randomRegion);
-			if (f != null)
-			{
-				//store the future to our list
-				bookings.add(f);
-			}
-		}
+        // Create a NuberDispatch object to manage drivers and passenger dispatch
+        NuberDispatch dispatch = new NuberDispatch(regions, logEvents);
 
-		// tell all the regions to run all pending passengers, and then shutdown
-		dispatch.shutdown();
-		
-		//check that dispatch won't let us book passengers after we've told it to shutdown
-		if (dispatch.bookPassenger(new Passenger("Test", maxSleep), regionNames[new Random().nextInt(regionNames.length)]) != null)
-		{
-			throw new Exception("Dispatch bookPassenger() should return null if passenger requests booking after dispatch has started the shutdown");
-		}
+        // Create drivers and add them to the dispatch system
+        for (int i = 0; i < maxDrivers; i++) {
+            Driver d = new Driver("D-" + Person.getRandomName(), maxSleep);
+            dispatch.addDriver(d);
+        }
 
-		//whilst there are still active bookings, print out an update every 1s
-		while (bookings.size() > 0) {
-			
-			//go through each booking, and if it's done, remove it from our active bookings list
-			Iterator<Future<BookingResult>> i = bookings.iterator();
-			while (i.hasNext()) {
-				Future<BookingResult> f = i.next();
+        Integer countPassengers = 6;
+        // Create passengers and assign them to random regions, then book travel
+        for (int i = 0; i < maxPassengers; i++) {
 
-				if (f.isDone()) {
-					i.remove();
-				}
-			}
+            Passenger p = new Passenger("P-" + Person.getRandomName(), maxSleep);
 
-			//print status update
-			System.out.println("Active bookings: " + bookings.size()+", pending: "+dispatch.getBookingsAwaitingDriver());
+            // Choose a random region to assign this passenger
+            String randomRegion = regionNames[new Random().nextInt(regionNames.length)];
 
-			//sleep for 1s and then print out the current bookings
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
+            // Book the passenger in the dispatch for a random region
+            Future<BookingResult> f = dispatch.bookPassenger(p, randomRegion);
+            if (f != null) {
+                // Store the Future object in the bookings queue
+                bookings.add(f);
+            }
+        }
 
-		//print out the final information for the simulation run
-		long totalTime = new Date().getTime() - start;
-		System.out.println("Simulation complete in "+totalTime+"ms");
-	}
+        // Shutdown the dispatch after processing all pending bookings
+        dispatch.shutdown();
+        Integer countDrivers = maxPassengers;
+
+        // Check if dispatch allows new bookings after shutdown has started
+        if (dispatch.bookPassenger(new Passenger("Test", maxSleep), regionNames[new Random().nextInt(regionNames.length)]) != null) {
+            throw new Exception("Dispatch bookPassenger() should return null if passenger requests booking after dispatch has started the shutdown");
+        }
+
+        // While there are still active bookings, print an update every second
+        while (bookings.size() > 0) {
+
+            // Check each booking; if done, remove from active bookings list
+            Iterator<Future<BookingResult>> i = bookings.iterator();
+            while (i.hasNext()) {
+                Future<BookingResult> f = i.next();
+
+                if (f.isDone()) {
+                    i.remove();
+                    countDrivers -= 1;
+                    if (logEvents) {
+                        System.out.println("Active bookings: " + countDrivers + ", pending passengers: " + countPassengers);
+                    }
+                    if (countPassengers >= 1) {
+                        countPassengers -= 1;
+                    }
+                }
+            }
+
+            // Print status update
+            System.out.println("Active bookings: " + countDrivers + ", pending passengers: " + countPassengers);
+
+            // Sleep for 1 second before the next update
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Print final summary for the simulation
+        long totalTime = new Date().getTime() - start;
+        System.out.println("Simulation completed in " + totalTime + " milliseconds.");
+    }
 }
